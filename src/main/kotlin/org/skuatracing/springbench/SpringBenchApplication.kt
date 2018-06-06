a6c2325e7ad844debfeb62a4306cd7402d2a10bc
Andrew Sun <me@andrewsun.com>
2018-06-06 14:42:47 -0400

    Add Jaeger

--- src/main/kotlin/org/skuatracing/springbench/SpringBenchApplication.kt
+++ src/main/kotlin/org/skuatracing/springbench/SpringBenchApplication.kt
@@ -1,11 +1,12 @@
 package org.skuatracing.springbench
 
+import io.jaegertracing.Tracer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
-
 
 data class MeowResponse(val meows: Long);
 
@@ -20,7 +21,12 @@ class MeowController {
 }
 
 @SpringBootApplication
-class SpringBenchApplication
+class SpringBenchApplication {
+    @Bean
+    fun tracer(): io.opentracing.Tracer {
+        return Tracer.Builder("spring-bench").build()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<SpringBenchApplication>(*args)
